698d47484292ea56fbbf4c854e43e60b5a47cdb2
Chilly712 <chillinthestar@outlook.com>
2022-04-19 16:17:39 -0500

    Simple clock demo to showcase `React.js`

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Timestamp />
+        <Timestamp displayText={"props passed from App.js"}/>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/component/timestamp.js
+++ src/component/timestamp.js
@@ -1,30 +1,46 @@
 import {useState, useEffect} from 'react';
 import React from 'react';
 import {getTimestamp} from './timestampServiceUtil';
+import PropType from 'prop-types'
 
-const Timestamp = () => {
+const Timestamp = (props) => {
+  Timestamp.PropType = {
+    displayText: PropType.string,
+  }
+
   const [time, setTime] = useState('');
+  const [hasLoad, setHasLoad] = useState(false);
 
   useEffect(() => {
+    if (!hasLoad) {
+      console.log("Page loading for the first time");
+      setHasLoad(true);
+    } else {
+      console.log("Page re-rendered due to state change");
+    }
+
     const tick = async () => {
       const t = await getTimestamp();
       setTime(t.timestamp);
 
-      // // the statement above is same as
+      /* the statement above is same as
       // getTimestamp().then(t => setTime(t.timestamp));
+      */
     };
-    const timerRefresh = setInterval(
-      () => tick(), 500);
 
-    return function cleanup() {
-      clearInterval(timerRefresh);
-    };
-  });
+    const timerRefresh = setInterval(() => tick(), 500);
+
+    /* Optional cleanup function */
+    // return function cleanup() {
+    //   clearInterval(timerRefresh);
+    // };
+  }, [time]);
 
   return (
     <div className="clock">
       <div className="screen">
         <h1 className="time">{time}</h1>
+        <p>{props.displayText}</p>
       </div>
     </div>
   );
